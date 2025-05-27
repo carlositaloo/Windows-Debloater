a633919c9e6979a1266af7455af14ea43c42c357
Carlos Ítalo <85812995+carlositaloo@users.noreply.github.com>
2025-05-27 11:56:22 -0300

    Resolução bug Explorer.exe

--- UninstallApps.ps1
+++ UninstallApps.ps1
@@ -8,7 +8,7 @@ Set-ItemProperty -Path "HKCU:\Software\Microsoft\GameBar" -Name "ShowStartupPane
 [regex]$WhitelistedApps = 'Microsoft.WindowsStore|Microsoft.GetHelp|Microsoft.Windows.Photos|Microsoft.WindowsCalculator|Microsoft.ScreenSketch|Microsoft.WindowsSoundRecorder|Microsoft.DesktopAppInstaller|Microsoft.WindowsCamera|NVIDIACorp.NVIDIAControlPanel|Microsoft.Paint|Microsoft.MicrosoftEdge.Stable|Microsoft.WindowsNotepad|Microsoft.XboxIdentityProvider|Microsoft.ZuneMusic|MicrosoftCorporationII.QuickAssist|WinRAR.ShellExtension|Microsoft.WindowsTerminal|Microsoft.VP9VideoExtensions|Microsoft.HEVCVideoExtension|Microsoft.WebMediaExtensions'
 
 # Obtém a lista de pacotes de aplicativos instalados e remove os não listados
-Get-AppxPackage -AllUsers | Where-Object {$_.Name -NotMatch $WhitelistedApps} | ForEach-Object { 
+Get-AppxPackage -AllUsers | Where-Object {$_.Name -NotMatch $WhitelistedApps -and $_.NonRemovable -ne $true} | ForEach-Object { 
     Remove-AppxPackage -Package $_.PackageFullName -ErrorAction SilentlyContinue
 }
 
